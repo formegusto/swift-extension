32ef91f24a0395dbf1acb3d263b7181b1da363c0
formegusto <formegusto@gmail.com>
2022-01-28 00:08:18 +0900

    initial commit

---
+++ carousel/carousel/Controllers/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  carousel
+//
+//  Created by formegusto on 2022/01/28.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
